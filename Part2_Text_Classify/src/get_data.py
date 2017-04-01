bb722715768187220a321eab294ec07c63268481
Roshanson <736781877@qq.com>
2017-04-01 10:40:31 +0800

    update Part2

---
+++ Part2_Text_Classify/src/get_data.py
@@ -0,0 +1,151 @@
+# encoding:utf-8
+
+import os
+import pandas as pd
+import sys
+import jieba
+import re
+import string
+import random
+from collections import Counter
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import MultiLabelBinarizer
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+base_path = "C:/Users/kaifun/Desktop/ass_TIP/TextInfoExp/Part2_Text_Classify/data_valid/"
+filelist = os.listdir(base_path)
+
+
+# 将文本汇总成一个txt
+def get_text():
+    data_dict = {}
+    for files in filelist:
+        # print (files)
+        f = open(base_path + files, 'r')
+        text = (f.read().decode('GB2312', 'ignore').encode('utf-8')).replace('\n', '')
+
+        data_temp = text.decode('utf-8')  # 转换为unicode编码形式
+        data = ''.join(re.findall(u'[\u4e00-\u9fff]+', data_temp))  # 必须为unicode类型，取出所有中文字符
+        data2 = jieba.cut(data)  # 分词
+        data3 = " ".join(data2)  # 结果转换为字符串（列表转换为字符串）
+        data_dict[data3] = "Art"
+
+    f2 = open('../data/art_res2.txt', 'a+')
+    for (k, v) in data_dict.items():
+        f2.write(v + ',' + k + ' ' + '\n')
+    f2.close()
+
+
+# 获取数据和标记
+def load_data():
+    data = pd.read_table('C:/Users/kaifun/Desktop/ass_TIP/TextInfoExp/Part2_Text_Classify/data/art_res2.txt',
+                         header=None, sep=',')
+    data2 = pd.read_table('C:/Users/kaifun/Desktop/ass_TIP/TextInfoExp/Part2_Text_Classify/data/computer_res2.txt',
+                          header=None, sep=',')
+    data3 = pd.read_table('C:/Users/kaifun/Desktop/ass_TIP/TextInfoExp/Part2_Text_Classify/data/sports_res2.txt',
+                          header=None, sep=',')
+    # print (data,data2,data3)
+
+    posting_list = []
+    class_list = []
+
+    for i in range(len(data)):
+        posting_list.append((data.iloc[i, 1]))
+        class_list.append(str(1))
+    for i in range(len(data2)):
+        posting_list.append((data2.iloc[i, 1]))
+        class_list.append(str(2))
+    for i in range(len(data3)):
+        posting_list.append((data3.iloc[i, 1]))
+        class_list.append(str(3))
+
+    # print (posting_list)
+    return posting_list, class_list
+
+
+def jieba_tokenizer(x): return jieba.cut(x, cut_all=True)
+
+
+def trans_text():
+    # salt = ''.join(random.sample(string.ascii_letters + string.digits, 8))
+    f3 = open('id2class2.txt', 'a')
+    for files in filelist:
+        # print (files)
+        f = open(base_path + files, 'r')
+        text = (f.read().decode('GB2312', 'ignore').encode('utf-8'))
+        salt = ''.join(random.sample(string.ascii_letters + string.digits, 8)) # 产生随机数
+        f2 = open("C:/Users/kaifun/Desktop/ass_TIP/TextInfoExp/Part2_Text_Classify/test3/" + salt + '.txt', 'w')
+        f2.write(text)
+        f3.write(salt + ' ' + 'e' + '\n')
+        f.close()
+        f2.close()
+
+
+def get_classify():
+    X_train, Y_train = load_data()
+
+    classifier = Pipeline([
+        ('counter', CountVectorizer(tokenizer=jieba_tokenizer)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', OneVsRestClassifier(LinearSVC())),
+    ])
+    mlb = MultiLabelBinarizer()
+    Y_train = mlb.fit_transform(Y_train)
+
+    classifier.fit(X_train, Y_train)
+
+    # X_test = ["数据分析"]
+    # 把所有的测试文本存到一个list中
+    test_list = []
+    test_name = []
+    filelist2 = os.listdir("C:/Users/kaifun/Desktop/ass_TIP/TextInfoExp/Part2_Text_Classify/data_test/")
+    for files in filelist2:
+        # print (files)
+        test_name.append(files)
+        f = open("C:/Users/kaifun/Desktop/ass_TIP/TextInfoExp/Part2_Text_Classify/data_test/" + files, 'r')
+        test_list.append(f.read())
+
+    prediction = classifier.predict(test_list)
+    result = mlb.inverse_transform(prediction)
+
+    f = open('result2.txt', 'w')
+    for i in range(len(test_name)):
+        f.write(str(test_name[i]) + '   ' + str(result[i]) + '\n')
+
+    print (result, len(result))
+    num_dict = Counter(result)
+    print (len(num_dict))
+    print ((num_dict[('1',)] + num_dict[('2',)] + num_dict[('3',)]) / float(len(result)))  # 整数除整数为0，应把其中一个改为浮点数。
+
+
+def get_text2():
+    for files in filelist:
+        f = open(base_path + files, 'r')
+        data = f.read()
+        f2 = open("C:/Users/kaifun/Desktop/ass_TIP/TextInfoExp/Part2_Text_Classify/test/" + files + '.txt', 'w')
+        f2.write(data)
+
+
+def check():
+    filename = []
+    print (filelist)
+    data = pd.read_table('C:/Users/kaifun/Desktop/ass_TIP/TextInfoExp/Part2_Text_Classify/src/result.txt', header=None,
+                         sep='\t')
+
+    for i in range(len(data)):
+         filename.append(str(data.iloc[i,0]).split()[0])
+
+    print (filename)
+
+
+if __name__ == '__main__':
+    # get_text()
+    # trans_text()
+    get_classify()
+    # get_text2()
+    # check()
